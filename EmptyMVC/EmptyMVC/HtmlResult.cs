5ced30b9eeb4d4f6127d68aae88ce10c22eefbd9
Selemeniv Roman <solomenckov.petya@ya.ru>
2023-03-19 17:21:49 +0300

    add mvc pattern

---
+++ EmptyMVC/EmptyMVC/HtmlResult.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmptyMVC
+{
+    public class HtmlResult : IActionResult
+    {
+        private string htmlCode;
+        public HtmlResult(string html) => htmlCode = html;
+        public async Task ExecuteResultAsync(ActionContext context)
+        {
+            string fullHtmlCode = @$"<!DOCTYPE html>
+            <html>
+                <head>
+                    <title>METANIT.COM</title>
+                    <meta charset=utf-8 />
+                </head>
+                <body>{htmlCode}</body>
+            </html>";
+            await context.HttpContext.Response.WriteAsync(fullHtmlCode);
+        }
+    }
+}
